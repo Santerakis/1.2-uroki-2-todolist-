b9aa8fc815baf09c0b578dca913ea7504dee1b44
santerakis <santerakis@gmail.com>
2022-11-18 15:46:26 +0200

    added map

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ function App() {
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
         { id: 3, title: "ReactJS", isDone: false },
-
+        { id: 4, title: "Rest API", isDone: false },
+        { id: 5, title: "GraphQL", isDone: false }
     ]
     const tasks2 = [
         { id: 1, title: "Hello world", isDone: true },
